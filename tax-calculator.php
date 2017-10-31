4d177da9335dfd79b8c44ddb18b40f7f8d500e66
milyasyousuf <m_ilyas@outlook.com>
2017-10-31 23:52:21 +0500

    bug fixed

--- tax-calculator.php
+++ tax-calculator.php
@@ -19,13 +19,15 @@ function tax_calculator( $atts ) {
 <section>
 
     <div class="calculator-div">
-        <h2>Tax Calculator</h2>
+
         <div id="pricecal">
-            <!--<h1>Price Calculator </h1>-->
+
             <form name="pricecal1">
-                <div style="border:1px dotted #fff; margin-top:5px; padding:5px;">
-                    <label></label><input type="text" name="area1" id="area1" placeholder="Enter your salary here" class="cal1" onkeyup="pricecal()"; value="1"> <br>
-                </div>
+                <div>
+                    <div style="border:5px dotted #fff; margin-top:10px; padding:10px;">
+                        <input type="text" name="area1" id="area1" placeholder="Enter your salary here" class="cal1" onkeyup="pricecal()" value="1" style="background-color: white;background-position: 10px 10px;padding: 10px;";"> <br></div>
+
+
             </form>
             <table>
                 <tr>
@@ -82,126 +84,126 @@ function tax_calculator( $atts ) {
 
 
 
-      
-    </div>
+
+        </div>
 
 
 
-    <?php }
-    function tax_calculator_script() {?>
+        <?php }
+        function tax_calculator_script() {?>
 
 
-        <!--calculator=========-->
-        <script type="text/javascript">
+            <!--calculator=========-->
+            <script type="text/javascript">
 
-            function pricecal(){
-                var str=pricecal1.area1.value;
-                str=str.slice(-1);
-                if(isNaN(str)==true){
-                    alert("Enter only numeric values");
-                    str=str.slice(0, -1);
-                    pricecal1.area1.value;
-                    return false;
+                function pricecal(){
+                    var str=pricecal1.area1.value;
+                    str=str.slice(-1);
+                    if(isNaN(str)==true){
+                        alert("Enter only numeric values");
+                        str=str.slice(0, -1);
+                        pricecal1.area1.value;
+                        return false;
+                    }
+                    var parea=parseFloat(pricecal1.area1.value);
+                    //var prate=parseFloat(pricecal1.rate1.value);
+                    var salary=parea.toFixed(2);
+                    var yearlySalary = salary * 12;
+                    var fixedTax =0;
+                    var taxPercent = 0;
+                    var limit = 0;
+                    var annualTax = 0;
+                    var montlyTax = 0;
+                    //alert(yearlySalary);
+                    if(yearlySalary<=400000){
+                        fixedTax =0;
+                        taxPercent = 0;
+                        limit = 0;
+
+                    }
+                    else if(yearlySalary>400000 && yearlySalary<=500000){
+                        fixedTax =0;
+                        taxPercent = 0.02;
+                        limit = 400000;
+
+                    }
+                    else if(yearlySalary>500000 && yearlySalary<=750000){
+                        fixedTax =2000;
+                        taxPercent = 0.05;
+                        limit = 500000;
+                    }
+                    else if(yearlySalary>750000 && yearlySalary<=1400000){
+                        fixedTax =14500;
+                        taxPercent = 0.1;
+                        limit = 750000;
+                    }
+                    else if(yearlySalary>1400000 && yearlySalary<=1500000){
+                        fixedTax =79500;
+                        taxPercent = 0.12;
+                        limit = 1400000;
+                    }
+                    else if(yearlySalary>1500000 && yearlySalary<=1800000){
+                        fixedTax =92000;
+                        taxPercent = 0.15;
+                        limit = 1500000;
+                    }
+                    else if(yearlySalary>1800000 && yearlySalary<=2500000){
+                        //tax = 137000+(salary-1800000)*0.175;
+                        fixedTax =137000;
+                        taxPercent = 0.175;
+                        limit = 1800000;
+                    }
+                    else if(yearlySalary>2500000 && yearlySalary<=3000000){
+
+                        fixedTax =259500;
+                        taxPercent = 0.2;
+                        limit = 2500000;
+
+                    }
+                    else if(yearlySalary>3000000 && yearlySalary<=3500000){
+                        fixedTax =359500;
+                        taxPercent = 0.22;
+                        limit = 3000000;
+                    }
+                    else if(yearlySalary>3500000 && yearlySalary<=4000000){
+                        fixedTax =472000;
+                        taxPercent = 0.25;
+                        limit = 3500000;
+                    }
+                    else if(yearlySalary>4000000 && yearlySalary<=7000000){
+                        fixedTax =597000;
+                        taxPercent = 0.27;
+                        limit = 4000000;
+                    }
+                    else if(yearlySalary>=7000000){
+                        fixedTax =1422000;
+                        taxPercent = 0.3;
+                        limit = 7000000;
+                    }
+
+
+                    annualTax = ((yearlySalary-limit)*taxPercent)+fixedTax;
+                    montlyTax = (annualTax/12) ;
+
+                    document.getElementById('priceresult').innerHTML=(salary);
+                    document.getElementById('tax').innerHTML=montlyTax.toFixed(2);
+                    document.getElementById('remainingSalary').innerHTML=(salary-montlyTax).toFixed(2);
+                    document.getElementById('yearlySalary').innerHTML=yearlySalary;
+                    document.getElementById('yearlyTax').innerHTML=(annualTax).toFixed(2);
+                    document.getElementById('yearlySalaryAfterTax').innerHTML=(yearlySalary-annualTax).toFixed(2);
+
                 }
-                var parea=parseFloat(pricecal1.area1.value);
-                //var prate=parseFloat(pricecal1.rate1.value);
-                var salary=parea.toFixed(2);
-                var yearlySalary = salary * 12;
-                var fixedTax =0;
-                var taxPercent = 0;
-                var limit = 0;
-                var annualTax = 0;
-                var montlyTax = 0;
-                //alert(yearlySalary);
-                if(yearlySalary<=400000){
-                    fixedTax =0;
-                    taxPercent = 0;
-                    limit = 0;
-
-                }
-                else if(yearlySalary>400000 && yearlySalary<=500000){
-                    fixedTax =0;
-                    taxPercent = 0.02;
-                    limit = 400000;
-
-                }
-                else if(yearlySalary>500000 && yearlySalary<=750000){
-                    fixedTax =2000;
-                    taxPercent = 0.05;
-                    limit = 500000;
-                }
-                else if(yearlySalary>750000 && yearlySalary<=1400000){
-                    fixedTax =14500;
-                    taxPercent = 0.1;
-                    limit = 750000;
-                }
-                else if(yearlySalary>1400000 && yearlySalary<=1500000){
-                    fixedTax =79500;
-                    taxPercent = 0.12;
-                    limit = 1400000;
-                }
-                else if(yearlySalary>1500000 && yearlySalary<=1800000){
-                    fixedTax =92000;
-                    taxPercent = 0.15;
-                    limit = 1500000;
-                }
-                else if(1800000>yearlySalary && yearlySalary<=2500000){
-                    //tax = 137000+(salary-1800000)*0.175;
-                    fixedTax =137000;
-                    taxPercent = 0.175;
-                    limit = 1800000;
-                }
-                else if(yearlySalary>2500000 && yearlySalary<=3000000){
-
-                    fixedTax =259500;
-                    taxPercent = 0.2;
-                    limit = 2500000;
-
-                }
-                else if(yearlySalary>3000000 && yearlySalary<=3500000){
-                    fixedTax =359500;
-                    taxPercent = 0.22;
-                    limit = 3000000;
-                }
-                else if(yearlySalary>3500000 && yearlySalary<=4000000){
-                    fixedTax =472000;
-                    taxPercent = 0.25;
-                    limit = 3500000;
-                }
-                else if(yearlySalary>4000000 && yearlySalary<=7000000){
-                    fixedTax =597000;
-                    taxPercent = 0.27;
-                    limit = 4000000;
-                }
-                else if(yearlySalary>=7000000){
-                    fixedTax =1422000;
-                    taxPercent = 0.3;
-                    limit = 7000000;
-                }
+            </script>
 
 
-                annualTax = ((yearlySalary-limit)*taxPercent)+fixedTax;
-                montlyTax = (annualTax/12) ;
+        <?php }
+        // Add hook for front-end <head></head>
+        add_action('wp_head', 'tax_calculator_script');
 
-                document.getElementById('priceresult').innerHTML=(salary);
-                document.getElementById('tax').innerHTML=montlyTax.toFixed(2);
-                document.getElementById('remainingSalary').innerHTML=(salary-montlyTax).toFixed(2);
-                document.getElementById('yearlySalary').innerHTML=yearlySalary;
-                document.getElementById('yearlyTax').innerHTML=(annualTax).toFixed(2);
-                document.getElementById('yearlySalaryAfterTax').innerHTML=(yearlySalary-annualTax).toFixed(2);
-
+        add_action('wp_footer', 'tax_calculator_scripts');
+        function tax_calculator_scripts() {
+            if (!is_admin()) {
+                wp_enqueue_script( 'jquery' );
             }
-        </script>
-
-
-    <?php }
-    // Add hook for front-end <head></head>
-    add_action('wp_head', 'tax_calculator_script');
-
-    add_action('wp_footer', 'tax_calculator_scripts');
-    function tax_calculator_scripts() {
-        if (!is_admin()) {
-            wp_enqueue_script( 'jquery' );
         }
-    }
-    ?>
+        ?>
